74d99148734cb0539a73449c78995f263ab45128
Maarten van der Glas <maartenvanderglas@gmail.com>
2024-03-08 11:46:34 +0100

    correct Resizing of window and emoji

--- src/components/EmojiPicker.svelte
+++ src/components/EmojiPicker.svelte
@@ -6,7 +6,7 @@
 
   const dispatch = createEventDispatcher();
   let pickerContainer;
-
+  let picker;
   function handleEmojiClick(event) {
     dispatch('select', event.detail.unicode);
     focusedEmojiIndex.set(null);
@@ -14,7 +14,7 @@
 
   onMount(async () => {
     await tick(); // Wait for the DOM to be updated
-    const picker = new Picker();
+    picker = new Picker();
     const shadow = picker.shadowRoot;
     const  style = document.createElement('style');
     style.textContent = '.search-row { display: none; }';
@@ -25,22 +25,37 @@
     shadow.appendChild(style2);
 
 
-    picker.style.setProperty('--num-columns', '12');
-    picker.style.setProperty('--emoji-size', `${window.innerHeight * 0.9 / 12}px`);
+    // picker.style.setProperty('--num-columns', '12');
+    // picker.style.setProperty('--emoji-size', `${window.innerHeight * 0.9 / 12}px`);
     picker.style.setProperty('--background', 'white');
     pickerContainer.appendChild(picker);
     picker.addEventListener('emoji-click', handleEmojiClick);
 
-    picker.style.height = '70vh';
-    picker.style.width = '80vw';
+    sizeEmoji();
 
     window.addEventListener('resize', () => {
+      sizeEmoji();
+  });
+});
+
+  function sizeEmoji(){
+    const aspektRatio = window.innerHeight / window.innerWidth;
+      if (aspektRatio > 1) {
+        picker.style.setProperty('--num-columns', '10');
+        picker.style.setProperty('--emoji-size', `${window.innerWidth  * 0.4  / 12}px`);
+      } else {
+        picker.style.setProperty('--num-columns', '10');
+        picker.style.setProperty('--emoji-size', `${window.innerWidth * 0.8 / 12}px`);
+      }
+
       // picker.style.setProperty('--emoji-size', `${window.innerHeight * 0.9 / 12}px`);
       picker.style.height = '70vh';
       picker.style.width = '80vw';
 
-    });
-  });
+  }
+
+
+
 
   onDestroy(() => {
     const picker = pickerContainer.querySelector('emoji-picker');
